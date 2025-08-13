8dd949b045fcaa052f3c61b36e75c4e4ffc6fe89
yoursongrequest <yoursongrequestdotcom@gmail.com>
2025-08-13 09:09:46 +0100

    Delete horizons-export-edcf8d21-9946-44ab-aa03-23ffecd7412e-5 directory

--- horizons-export-edcf8d21-9946-44ab-aa03-23ffecd7412e-5/src/lib/customSupabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://xyplbhuqfihdptmffvvp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5cGxiaHVxZmloZHB0bWZmdnZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5OTYwNTYsImV4cCI6MjA3MDU3MjA1Nn0.CVMRAMB3M1BtRE_b__f2KEUr0Cm80WpOELbFAFGCtM8';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
